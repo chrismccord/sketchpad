0709daf4320b4632dac52303c77c3d3114c2a7bb
Chris McCord <chris@chrismccord.com>
2017-04-30 17:44:32 -0400

    Upgrade to Phoenix 1.3

--- lib/sketchpad.ex
+++ lib/sketchpad.ex
@@ -1,19 +1,5 @@
-defmodule Sketchpad.Application do
-  use Application
-
-  # See http://elixir-lang.org/docs/stable/elixir/Application.html
-  # for more information on OTP Applications
-  def start(_type, _args) do
-    import Supervisor.Spec
-
-    # Define workers and child supervisors to be supervised
-    children = [
-      supervisor(Sketchpad.Web.Endpoint, [])
-    ]
-
-    # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
-    # for other strategies and supported options
-    opts = [strategy: :one_for_one, name: Sketchpad.Supervisor]
-    Supervisor.start_link(children, opts)
-  end
+defmodule Sketchpad do
+  @moduledoc """
+  Collaborative Sketchpad.
+  """
 end

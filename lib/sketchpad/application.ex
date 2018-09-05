f1881bb8fbcf8a4dd22a2d6fdb05f9b04960fc44
Chris Freeze <chrstnfreeze@gmail.com>
2018-09-05 14:14:34 -0700

    Real genserver

--- lib/sketchpad/application.ex
+++ lib/sketchpad/application.ex
@@ -9,7 +9,7 @@ defmodule Sketchpad.Application do
     # Define workers and child supervisors to be supervised
     children = [
       SketchpadWeb.Endpoint,
-      Counter
+      {Counter, 0}
     ]
 
     # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
--- lib/sketchpad/counter.ex
+++ lib/sketchpad/counter.ex
@@ -1,48 +1,30 @@
 defmodule Counter do
-  def inc(pid), do: send(pid, :inc)
-  def dec(pid), do: send(pid, :dec)
+  use GenServer
+  def inc(pid), do: GenServer.cast(pid, :inc)
+  def dec(pid), do: GenServer.cast(pid, :dec)
 
   def val(pid, timeout \\ 5000) do
-    ref = Kernel.make_ref()
-    send(pid, {:val, self(), ref})
-
-    receive do
-      {:val, value, ^ref} ->
-        value
-    after
-      timeout -> exit(:timeout)
-    end
+    GenServer.call(pid, :val, timeout)
   end
 
   def start_link(initial_count \\ 0) do
-    {:ok, spawn_link(__MODULE__, :init, [initial_count])}
+    GenServer.start_link(__MODULE__, [initial_count], name: __MODULE__)
   end
 
-  def init(initial_count) do
-    :timer.send_interval(1000, self(), :tick)
-    run(initial_count)
+  def init([initial_count]) do
+    {:ok, initial_count}
   end
 
-  defp run(count) do
-    receive do
-      message ->
-        message
-        |> handle_in(count)
-        |> run()
-    end
+  def handle_cast(:inc, count), do: {:noreply, count + 1}
+  def handle_cast(:dec, count), do: {:noreply, count - 1}
+
+  def handle_call(:val, _from, count) do
+    {:reply, count, count}
   end
 
-  def handle_in(:inc, count), do: count + 1
-  def handle_in(:dec, count), do: count - 1
-
-  def handle_in({:val, caller, ref}, count) do
-    send(caller, {:val, count, ref})
-    count
-  end
-
-  def handle_in(:tick, count) do
+  def handle_info(:tick, count) do
     count = count + 1
     if count > 5, do: raise("boom")
-    IO.inspect(count)
+    {:noreply, IO.inspect(count)}
   end
 end

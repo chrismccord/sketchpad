5e7585df0a6900faffca1c80299cfc02bcc20179
Chris McCord <chris@chrismccord.com>
2018-09-05 11:32:28 -0700

    Checkpoint

--- assets/js/app.js
+++ assets/js/app.js
@@ -18,13 +18,35 @@ let App = {
     this.el = document.getElementById("sketchpad")
     this.pad = new Sketchpad(this.el, window.username)
 
-    // do stuff
-
-    this.padChannel.on("tick", ({value}) => console.log("tick", value))
+    this.bind()
 
     this.padChannel.join()
       .receive("ok", resp => console.log("joined!", resp))
       .receive("error", resp => console.log("failed join", resp))
+  },
+
+  bind(){
+    this.pad.on("stroke", data => this.padChannel.push("stroke", data))
+
+    this.padChannel.on("stroke", ({user_id, stroke}) => {
+      this.pad.putStroke(user_id, stroke, {color: "#000000"})
+    })
+
+    this.clearButton = document.getElementById("clear-button")
+    this.exportButton = document.getElementById("export-button")
+
+    this.clearButton.addEventListener("click", e => {
+      e.preventDefault()
+      this.padChannel.push("clear", {})
+    })
+
+    this.padChannel.on("clear", () => this.pad.clear())
+
+    this.exportButton.addEventListener("click", e => {
+      let win = window.open()
+      win.document.write(`<img src="${this.pad.getImageURL()}"/>`)
+    })
+
   }
 }
 if(window.userToken !== ""){ App.init() }
--- lib/sketchpad_web/channels/pad_channel.ex
+++ lib/sketchpad_web/channels/pad_channel.ex
@@ -1,20 +1,38 @@
 defmodule SketchpadWeb.PadChannel do
   use SketchpadWeb, :channel
 
-  def join("pad:" <> pad_id, _params, socket) do
-    :timer.send_interval(1000, self(), :count)
+  def topic(pad_id), do: "pad:#{pad_id}"
 
+  def broadcast_stroke_from(from, pad_id, user_id, stroke) do
+    SketchpadWeb.Endpoint.broadcast_from!(from, topic(pad_id), "stroke", %{
+      user_id: user_id,
+      stroke: stroke
+    })
+  end
+
+  def join("pad:" <> pad_id, _params, socket) do
     socket =
       socket
       |> assign(:pad_id, pad_id)
-      |> assign(:count, 0)
 
     {:ok, socket}
   end
 
-  def handle_info(:count, socket) do
-    if socket.assigns.count > 5, do: raise "boom"
-    push(socket, "tick", %{value: socket.assigns.count})
-    {:noreply, assign(socket, :count, socket.assigns.count + 1)}
+  def handle_in("stroke", data, socket) do
+    %{user_id: user_id, pad_id: pad_id} = socket.assigns
+    broadcast_stroke_from(self(), pad_id, user_id, data)
+
+    {:reply, :ok, socket}
+  end
+
+  def broadcast_clear(pad_id) do
+    pad_id
+    |> topic()
+    |> SketchpadWeb.Endpoint.broadcast!("clear", %{})
+  end
+
+  def handle_in("clear", _, socket) do
+    broadcast_clear(socket.assigns.pad_id)
+    {:reply, :ok, socket}
   end
 end
